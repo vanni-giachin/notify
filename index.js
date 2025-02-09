47eece330d74e44ee6d47a7698bd75c551bc14d4
Vanni Giachin <vanni.giachin@gmail.com>
2025-02-09 21:04:35 +0100

    Telegram channel

--- index.js
+++ index.js
@@ -1,43 +1,98 @@
+import {Bot} from 'https://bundle.deno.dev/https://raw.githubusercontent.com/grammyjs/grammY/v1.34.1/src/mod.ts';
+
 const valueElement = document.getElementById('value');
 const infoElement = document.getElementById('info');
-const inputElement = document.getElementById('input');
+const fileElement = document.getElementById('file');
 const testButton = document.getElementById('test');
 
-inputElement.addEventListener('change', function (e) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        const {url, value, delay} = JSON.parse(e.target.result);
-        console.log(url, value, delay);
-        startInterval(url, value, delay);
-    };
-    const file = e.target.files?.[0];
-    file && reader.readAsText(file);
-}, false);
-
-testButton.addEventListener('click', () => notify('Test!'));
-
-let serviceWorkerRegistration, counter = 0;
+let serviceWorkerRegistration, telegram, intervalId;
 
 Promise.all([
-    navigator.serviceWorker.register('worker.js'),
-    Notification.requestPermission()
-]).then(([r]) => serviceWorkerRegistration = r);
+    Notification.requestPermission(),
+    navigator.serviceWorker.register('worker.js')
+]).then(function ([permission, r]) {
+    serviceWorkerRegistration = r;
 
-const notify = (message) => serviceWorkerRegistration.showNotification(message);
+    if (permission !== 'granted') {
+        return;
+    }
 
-const startInterval = (url, value, delay) => setInterval((function f() {
-    fetch(url).then(r => r.text()).then(text => {
-        const time = new Date().toLocaleTimeString();
-        const info = `Response ${++counter} received at ${time}`;
-        valueElement.textContent = text;
-        infoElement.textContent = info;
+    const item = localStorage.getItem('configuration');
 
-        if (text !== value) {
-            return notify('New response returned!');
-        }
-        if (counter % 100 === 0) {
-            return notify('Test!');
-        }
-    });
-    return f;
-})(), Math.max(1000, delay));
+    if (item) {
+        start(JSON.parse(item));
+    }
+
+    fileElement.addEventListener('change', function (event) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            start(JSON.parse(e.target.result));
+
+            localStorage.setItem('configuration', e.target.result);
+        };
+
+        reader.readAsText(event.target.files[0]);
+    }, false);
+
+    fileElement.disabled = false;
+
+    testButton.addEventListener('click', () => notify('Test!'));
+
+    testButton.disabled = false;
+});
+
+function notify(message) {
+    if (telegram?.bot) {
+        telegram.bot.api.sendMessage(telegram.userId, message);
+    }
+
+    serviceWorkerRegistration.showNotification(message);
+}
+
+function start(configuration) {
+    const {url, value, delay, thresholds, ...channels} = configuration;
+
+    telegram = channels.telegram;
+
+    if (telegram) {
+        telegram.bot = new Bot(telegram.token)
+    }
+
+    const {error: errorThreshold, test: testThreshold} = thresholds || {};
+
+    let successCounter = 0, errorCounter = 0;
+
+    function fetchAndCompare() {
+        fetch(url).then(r => r.text()).then(text => {
+            errorCounter = 0;
+
+            valueElement.textContent = text;
+            infoElement.textContent = `Response ${++successCounter} received at ${getTimestamp()}`;
+
+            if (value !== text) {
+                return notify('New response returned!');
+            }
+
+            if (successCounter % testThreshold === 0) {
+                return notify('Test!');
+            }
+        }).catch(e => {
+            if (++errorCounter === errorThreshold) {
+                notify('Error!');
+            }
+
+            throw e;
+        });
+    }
+
+    function getTimestamp() {
+        return new Date().toLocaleTimeString();
+    }
+
+    fetchAndCompare();
+
+    clearInterval(intervalId);
+
+    intervalId = setInterval(fetchAndCompare, delay);
+}
